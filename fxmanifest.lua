476d7e3d7a7a42ca6742d656b6d4f2e693dfd0e4
myd <168150436+myd5m@users.noreply.github.com>
2026-05-15 23:07:56 +0200

    Refactor fxmanifest.lua syntax and structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,8 @@
-fx_version('cerulean')
-games('gta5')
+fx_version 'cerulean'
+game 'gta5'
+author 'myd'
+description 'No more bloom on your 5m server'
 
-author('myd')
-description('No more bloom on your 5m server')
-
-server_scripts(
-    "server.js"
-)
+server_scripts {
+    'server.js'
+}
